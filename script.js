e650373708f657715cc1be95144b3c92b7ce73b7
nekoeta <nekoeta2021@gmail.com>
2025-07-19 19:03:26 +0900

    計算結果を保存

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ function convertToBinary() {
 
   if (isNaN(number)) {
     document.getElementById("result").textContent = "数値を正しく入力してください。";
-    return true;
+    return false;
   }
 
   const binary = number.toString(2);
-  document.getElementById("result").textContent = `2進数：${binary}`;
+  document.getElementById("result").value = binary;
   document.getElementById("resultDisp").textContent = `2進数：${binary}`;
+  return true;
 }
